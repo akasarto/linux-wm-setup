482d37a23b105acf62f3e09511ef9b64e0fd2219
akasarto <5660350+akasarto@users.noreply.github.com>
2024-12-13 05:41:54 +0000

    Update setup script.

--- setup-i3.sh
+++ setup-i3.sh
@@ -4,10 +4,12 @@
 set -e
 set -u
 
-# reset
-cd ~
+# track work dir
+
+wdir=$(pwd)
 
 # recreate folders
+cd ~
 rm -rf .tools
 rm -rf .fonts
 mkdir .tools
@@ -18,42 +20,42 @@ sudo apt update
 sudo apt upgrade -y
 
 # install dependencies
-sudo apt install \
+sudo apt install -y \
     xserver-xorg-core \
     x11-xserver-utils \
     xinit \
     wget \
     i3-wm \
     rxvt-unicode \
-    exa
+    rofi
 
-# download and install 7zip
-cd .tools/
+# .Xresources
 
-rm -f /usr/bin/7z
+cd $wdir
+cp .Xresources ~
+
+# 7zip
+cd ~/.tools/
+
+sudo rm -f /usr/bin/7z
 
 wget https://www.7-zip.org/a/7z2409-linux-x64.tar.xz
 
 tar -xf 7z2409-linux-x64.tar.xz 7zz
 sudo ln -s ~/.tools/7zz /usr/bin/7z
 rm -f 7z2409-linux-x64.tar.xz
-cd ~
 
-# download and install fonts
-cd .fonts/
+# Cascadia Code Font
+cd ~/.fonts/
 
 wget https://github.com/microsoft/cascadia-code/releases/download/v2407.24/CascadiaCode-2407.24.zip
 
 7z e CascadiaCode-2407.24.zip ttf/*.ttf
 rm -f CascadiaCode-2407.24.zip
+
+# Microsoft Edge :p
 cd ~
-
-# copy .Xresource file for console theme
-
-cp ./Xresources ~/
-
-# download and install Microsoft Edge :p
 
 wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_131.0.2903.99-1_amd64.deb
 
-sudo apt install ~/microsoft-edge-stable_131.0.2903.99-1_amd64.deb
+sudo apt install -y ./microsoft-edge-stable_131.0.2903.99-1_amd64.deb
