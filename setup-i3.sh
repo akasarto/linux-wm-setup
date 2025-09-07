d7eb50c572b08a9001c2452807bd94d3f6307646
Thiago Schneider <5660350+akasarto@users.noreply.github.com>
2025-09-07 06:50:51 +0100

    Update setup-i3.sh

--- setup-i3.sh
+++ setup-i3.sh
@@ -1,64 +1,39 @@
-#!/usr/bin/env bash
+! xrdb ~/.Xresources
 
-# exit on errors
-set -e
-set -u
+! --------------------------------------
 
-# track work dir
+URxvt.font:                xft:CascadiaMonoNF:pixelsize=16:book
+URxvt.boldFont:            xft:CascadiaMonoNF:pixelsize=16:bold
+URxvt.italicFont:          xft:CascadiaMonoNFItalic:pixelsize=16:italic
+URxvt.boldItalicFont:      xft:CascadiaMonoNFItalic:pixelsize=16:bold:italic
 
-wdir=$(pwd)
+URxvt.letterSpace:         0
+URxvt.internalBorder:      0
+URxvt.externalBorder:      0
 
-# recreate folders
-cd ~
-rm -rf .tools
-rm -rf .fonts
-mkdir .tools
-mkdir .fonts
+URxvt.cursorBlink: 		     true
+URxvt.scrollBar:           false
 
-# update packages
-sudo apt update
-sudo apt upgrade -y
+URxvt.fading:              0
 
-# install dependencies
-sudo apt install -y \
-    curl \
-    wget \
-    xserver-xorg-core \
-    x11-xserver-utils \
-    mesa-utils \
-    xinit \
-    i3-wm \
-    rxvt-unicode \
-    thunar \
-    rofi \
-    zip \
-    unzip
+! --------------------------------------
 
-# .Xresources
+URxvt.foreground: #9e9e9e
+URxvt.background: #000000
 
-cd $wdir
-cp .Xresources ~
-
-# 7zip
-cd ~/.tools/
-
-sudo rm -f /usr/bin/7z
-
-wget https://www.7-zip.org/a/7z2409-linux-x64.tar.xz
-
-tar -xf 7z2409-linux-x64.tar.xz 7zz
-sudo ln -s ~/.tools/7zz /usr/bin/7z
-rm -f 7z2409-linux-x64.tar.xz
-
-# Cascadia Code Font
-cd ~/.fonts/
-
-wget https://github.com/microsoft/cascadia-code/releases/download/v2407.24/CascadiaCode-2407.24.zip
-
-7z e CascadiaCode-2407.24.zip ttf/*.ttf
-rm -f CascadiaCode-2407.24.zip
-
-# Brave
-cd ~
-
-curl -fsS https://dl.brave.com/install.sh | sh
+URxvt.color0:  #2E3436
+URxvt.color1:  #a40000
+URxvt.color2:  #4E9A06
+URxvt.color3:  #C4A000
+URxvt.color4:  #008a17
+URxvt.color5:  #75507B
+URxvt.color6:  #ce5c00
+URxvt.color7:  #babdb9
+URxvt.color8:  #555753
+URxvt.color9:  #EF2929
+URxvt.color10: #8AE234
+URxvt.color11: #af9155
+URxvt.color12: #729FCF
+URxvt.color13: #AD7FA8
+URxvt.color14: #fcaf3e
+URxvt.color15: #EEEEEC
